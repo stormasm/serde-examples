32ea62a4c5a942cb71a091ba66c8544c9a51ec0c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-01-03 12:29:38 -0800

    mw

--- ron/examples/ex02b.rs
+++ ron/examples/ex02b.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 
 #[allow(dead_code)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Point {
     x: i32,
 }
@@ -10,8 +10,8 @@ fn main() {
     let serialized = "(x:1)";
 
     // Convert the JSON string back to a Point.
-    let _deserialized: Point = ron::from_str(&serialized).unwrap();
+    let deserialized: Point = ron::from_str(&serialized).unwrap();
 
     // Prints deserialized = Point { x: 1}
-    // println!("deserialized = {:?}", deserialized);
+    println!("deserialized = {:?}", deserialized);
 }
